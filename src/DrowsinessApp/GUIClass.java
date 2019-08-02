05da557bd0fb0468d4a42666d4410b6f83764a49
Narin Kunaseth <narin.kunaseth@gmail.com>
2019-08-02 15:54:31 +0700

    change random transaction timer

--- src/DrowsinessApp/GUIClass.java
+++ src/DrowsinessApp/GUIClass.java
@@ -1123,7 +1123,7 @@ public class GUIClass extends javax.swing.JFrame {
                             }
                             stackData.add(keys.get(randIdx));
                         }
-                    }, (15 + new Random().nextInt(16)) * 1000, (15 + new Random().nextInt(16)) * 1000);
+                    }, (7 + new Random().nextInt(8)) * 1000, (7 + new Random().nextInt(8)) * 1000);
                     executor = Executors.newScheduledThreadPool(2);
                     executor.scheduleAtFixedRate(collectCursor, 0, 20, TimeUnit.MILLISECONDS);
                     executor.scheduleAtFixedRate(refreshData, 15, 15, TimeUnit.MINUTES);
